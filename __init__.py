428d003b80fa41cec1193dba29761e82931401b3
Chris Rohrer <chris.rohrer@outlook.com>
2017-11-13 15:47:56 +0100

    New Version of everything

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
-import random
-import time
-import webbrowser
-from os.path import dirname, join
+from os.path import dirname
 
 from adapt.intent import IntentBuilder
-from mycroft.messagebus.message import Message
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
@@ -16,33 +12,19 @@ LOGGER = getLogger(__name__)
 class QuotingSkill(MycroftSkill):
     def __init__(self):
         super(QuotingSkill, self).__init__(name="QuotingSkill")
-        self.process = None
-        quoteone = "Don't worry about what anybody else is going to do. The best way to predict the future is to invent it. By Alan Kay"
 
     def initialize(self):
-        intent = IntentBuilder("QuotingIntent").require(
-            "QuotingKeyword").build()
-        self.register_intent(intent, self.handle_intent)
-        self.add_event("mycroft.quote", self.quote, False)
+        self.load_data_files(dirname(__file__))
 
-    def quote(self, quoteone):
-        self.speak(quoteone)
+        quote_intent = IntentBuilder("QuoteIntent").\
+            require("QuoteKeyword").build()
+        self.register_intent(quote_intent, self.handle_quote_intent)
 
-    def handle_intent(self, message):
-        quotetwo = "Premature optimization is the root of all evil (or at least most of it) in programming. By Donald Knuth"
-        try:
-            self.speak_dialog('quoting')
-            time.sleep(3)
-            self.speak(quoteone)
+    def handle_thank_you_intent(self, message):
+        self.speak_dialog("quote")
 
-        except Exception as e:
-            LOGGER.error("Error: {0}".format(e))
-            
     def stop(self):
-        if self.process and self.process.poll() is None:
-            self.speak_dialog('quoting.stop')
-            self.process.terminate()
-            self.process.wait()
+        pass
 
 def create_skill():
     return QuotingSkill()
